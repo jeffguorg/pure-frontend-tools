86a8a1a1afe22869cb87ecb51ac8084c4796bed5
guochao <jeffguorg@gmail.com>
2020-12-02 21:05:09 +0800

    put up golang and node js code

--- src/views/PushNotification.tsx
+++ src/views/PushNotification.tsx
@@ -15,7 +15,12 @@ type NotificateState = {
     PushSubscriptionObject?: PushSubscription
 }
 
-export class PushNotification extends BaseComponent<TitleProp, NotificateState> {
+type PushNotificationPageButtonTextState = {
+    NodeJSCodeCopied?: boolean
+    GolangCodeCopied?: boolean
+}
+
+export class PushNotification extends BaseComponent<TitleProp, NotificateState & PushNotificationPageButtonTextState> {
     private supported = ('Notification' in window && !!navigator.serviceWorker);
 
     constructor(props: TitleProp) {
@@ -119,6 +124,55 @@ export class PushNotification extends BaseComponent<TitleProp, NotificateState> 
 
         if (this.state.PushSubscriptionObject) {
             let subscription = this.state.PushSubscriptionObject;
+            let NodeJSCode = `var webPush = require('web-push');
+
+var pushSubscription = ${JSON.stringify(subscription.toJSON(), null, 4)}
+var payload = 'Hi!';
+var options = {
+    publicKey: '${Buffer.from(localStorage.getItem("push.pubkey") || "", "hex").toString("base64").replaceAll("+", "-").replaceAll("/", "_").replaceAll("=", "")}', // Buffer.from("${localStorage.getItem("push.pubkey") || ""}", "hex").toString("base64").replaceAll("+", "-").replaceAll("/", "_").replaceAll("=", "")
+    privateKey: '${Buffer.from(localStorage.getItem("push.privkey") || "", "hex").toString("base64").replaceAll("+", "-").replaceAll("/", "_").replaceAll("=", "")}', // Buffer.from("${localStorage.getItem("push.privkey") || ""}", "hex").toString("base64").replaceAll("+", "-").replaceAll("/", "_").replaceAll("=", "")
+};
+
+webPush.sendNotification(
+    pushSubscription,
+    payload,
+    options
+);`
+
+            let GolangCode = `package main
+
+import (
+    "io/ioutil"
+    "log"
+
+    "github.com/SherClockHolmes/webpush-go"
+)
+
+func main() {
+    subscription := webpush.Subscription{
+        Endpoint: "${subscription.endpoint}",
+        Keys: webpush.Keys{
+            Auth:   "${(subscription.toJSON().keys as Record<string, string>)["auth"]}",
+            P256dh: "${(subscription.toJSON().keys as Record<string, string>)["p256dh"]}",
+        },
+    }
+    payload := []byte("Hi")
+    resp, err := webpush.SendNotification(payload, &subscription, &webpush.Options{
+        VAPIDPublicKey: "${Buffer.from(localStorage.getItem("push.pubkey") || "", "hex").toString("base64").replaceAll("+", "-").replaceAll("/", "_").replaceAll("=", "")}",
+        VAPIDPrivateKey:  "${Buffer.from(localStorage.getItem("push.privkey") || "", "hex").toString("base64").replaceAll("+", "-").replaceAll("/", "_").replaceAll("=", "")}",
+    })
+    if err != nil {
+        log.Fatal("failed to send request: ", err)
+    }
+    defer resp.Body.Close()
+    if resp.StatusCode/100 != 2 {
+        body, _ := ioutil.ReadAll(resp.Body)
+        log.Fatal("unexpected status code: ", resp.StatusCode, ", body: ", string(body))
+    }
+    log.Println("ok")
+}
+`
+
             return (
                 <Container>
                     <Row>
@@ -135,17 +189,179 @@ export class PushNotification extends BaseComponent<TitleProp, NotificateState> 
                                             </Row>
                                             <Row>
                                                 <Col>
-                                                    {JSON.stringify(subscription.toJSON())}
+                                                    Public Key: {localStorage.getItem("push.pubkey")}
+                                                </Col>
+                                            </Row>
+                                            <Row>
+                                                <Col>
+                                                    <pre>
+                                                        {JSON.stringify(subscription.toJSON(), null, 4)}
+                                                    </pre>
                                                 </Col>
                                             </Row>
                                         </Container>
-
                                     </Card.Body>
                                     <Card.Footer>
                                         <DefaultPadding><Button color="primary" onClick={() => {
                                             subscription.unsubscribe()
                                             this.queryNotificationState()
                                         }}>Unsubscribe</Button></DefaultPadding>
+                                    </Card.Footer>
+                                </Card>
+                            </DefaultPadding>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <DefaultPadding>
+                                <Card>
+                                    <Card.Header>Push With NodeJS(require web-push)</Card.Header>
+                                    <Card.Body>
+                                        <Container>
+                                            <Row>
+                                                <Col>
+                                                    <pre id="nodejs-code">
+                                                        {NodeJSCode}
+                                                    </pre>
+                                                </Col>
+                                            </Row>
+                                        </Container>
+                                    </Card.Body>
+                                    <Card.Footer>
+                                        <DefaultPadding>
+                                            {
+                                                this.state.NodeJSCodeCopied === undefined ? (
+                                                    <Button color="primary" onClick={() => {
+                                                        navigator.clipboard.writeText(NodeJSCode).then(() => {
+                                                            this.setState({
+                                                                NodeJSCodeCopied: true
+                                                            })
+                                                        }).catch(() => {
+                                                            let nodejsCode = document.getElementById("nodejs-code");
+                                                            if (nodejsCode) {
+                                                                nodejsCode.focus();
+                                                            }
+
+                                                            this.setState({
+                                                                NodeJSCodeCopied: false
+                                                            })
+                                                        })
+                                                    }}>Copy</Button>
+                                                ) : this.state.NodeJSCodeCopied ? (
+                                                    <Button color="primary" onClick={() => {
+                                                        navigator.clipboard.writeText(NodeJSCode).then(() => {
+                                                            this.setState({
+                                                                NodeJSCodeCopied: true
+                                                            })
+                                                        }).catch(() => {
+                                                            let nodejsCode = document.getElementById("nodejs-code");
+                                                            if (nodejsCode) {
+                                                                nodejsCode.focus();
+                                                            }
+
+                                                            this.setState({
+                                                                NodeJSCodeCopied: false
+                                                            })
+                                                        })
+                                                    }}>Done! Paste it to your IDE or click to Copy Again!</Button>
+                                                ) : (
+                                                            <Button color="warn" onClick={() => {
+                                                                navigator.clipboard.writeText(NodeJSCode).then(() => {
+                                                                    this.setState({
+                                                                        NodeJSCodeCopied: true
+                                                                    })
+                                                                }).catch(() => {
+                                                                    let nodejsCode = document.getElementById("nodejs-code");
+                                                                    if (nodejsCode) {
+                                                                        nodejsCode.focus();
+                                                                    }
+
+                                                                    this.setState({
+                                                                        NodeJSCodeCopied: false
+                                                                    })
+                                                                })
+                                                            }}>Unable to copy text. Click to try again</Button>
+                                                        )
+                                            }
+                                        </DefaultPadding>
+                                    </Card.Footer>
+                                </Card>
+                            </DefaultPadding>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <DefaultPadding>
+                                <Card>
+                                    <Card.Header>Push With Golang</Card.Header>
+                                    <Card.Body>
+                                        <Container>
+                                            <Row>
+                                                <Col>
+                                                    <pre id="golang-code">
+                                                        {GolangCode}
+                                                    </pre>
+                                                </Col>
+                                            </Row>
+                                        </Container>
+                                    </Card.Body>
+                                    <Card.Footer>
+                                        <DefaultPadding>
+                                            {
+                                                this.state.GolangCodeCopied === undefined ? (
+                                                    <Button color="primary" onClick={() => {
+                                                        navigator.clipboard.writeText(GolangCode).then(() => {
+                                                            this.setState({
+                                                                GolangCodeCopied: true
+                                                            })
+                                                        }).catch(() => {
+                                                            let golangCode = document.getElementById("golang-code");
+                                                            if (golangCode) {
+                                                                golangCode.focus();
+                                                            }
+
+                                                            this.setState({
+                                                                GolangCodeCopied: false
+                                                            })
+                                                        })
+                                                    }}>Copy</Button>
+                                                ) : this.state.GolangCodeCopied ? (
+                                                    <Button color="primary" onClick={() => {
+                                                        navigator.clipboard.writeText(GolangCode).then(() => {
+                                                            this.setState({
+                                                                GolangCodeCopied: true
+                                                            })
+                                                        }).catch(() => {
+                                                            let golangCode = document.getElementById("golang-code");
+                                                            if (golangCode) {
+                                                                golangCode.focus();
+                                                            }
+
+                                                            this.setState({
+                                                                GolangCodeCopied: false
+                                                            })
+                                                        })
+                                                    }}>Done! Paste it to your IDE or click to Copy Again!</Button>
+                                                ) : (
+                                                            <Button color="warn" onClick={() => {
+                                                                navigator.clipboard.writeText(GolangCode).then(() => {
+                                                                    this.setState({
+                                                                        GolangCodeCopied: true
+                                                                    })
+                                                                }).catch(() => {
+                                                                    let golangCode = document.getElementById("golang-code");
+                                                                    if (golangCode) {
+                                                                        golangCode.focus();
+                                                                    }
+
+                                                                    this.setState({
+                                                                        GolangCodeCopied: false
+                                                                    })
+                                                                })
+                                                            }}>Unable to copy text. Click to try again</Button>
+                                                        )
+                                            }
+                                        </DefaultPadding>
                                     </Card.Footer>
                                 </Card>
                             </DefaultPadding>
@@ -169,6 +385,7 @@ export class PushNotification extends BaseComponent<TitleProp, NotificateState> 
                                                 let [priv, pub] = [keypair.getPrivate(), keypair.getPublic()];
 
                                                 localStorage.setItem("push.privkey", priv.toJSON())
+                                                localStorage.setItem("push.pubkey", pub.encode("hex", false))
                                                 let encoded = Uint8Array.from(pub.encode("array", false)).buffer;
 
                                                 let sub = await reg.pushManager.subscribe({
